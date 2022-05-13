ab50f5e95896131fc2351cc7fe23ece5db2d614e
Quied <quiedcpp@gmail.com>
2022-05-13 12:40:03 +0300

    loop

--- main.rs
+++ main.rs
@@ -1,13 +1,19 @@
-fn main(){
-
-    let a = 42;
-    let d = 42;
+fn main() {
+        let a = 42;
+        let d = 42;
 
     let arr:[i32;4] = [24,42,24,42];
-    println!("Array {}", arr.len());
+    println!("Array size {}", arr.len());
+    let v = &["main", "this", "pointer"];
 
-
-        
-
-
+    let mut iter = arr.iter();
+   
+    
+    let next = iter.next();
+    for text in v{
+    println!("This {}.", text);
+    
+    }
+   
+   
 }
